26848db23093b2b950df2460021dee5ebd900e43
Marcant Aimé <aime.marcant@hotmail.fr>
2023-01-05 15:20:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# env-not-found
+
+Install with `npm install -g env-not-found`
+
+## How it works
+
+It searches all occurrences of "process.env.[value]" in your current dir ('.js', '.ts', '.jsx', '.tsx') and verifies if value is in the .env file.
+
+## Usage
+
+```
+env-not-found
+```
+
+If there are missing environment variables, the terminal output will be:
+```
+Warning: "COMMENT_WEBSOCKET_PORT" is not specified in the .env file
+```
+The program will then exit with a code of 1.
+
+<br>
+
+If all environment variables are present, nothing will be printed and the program will continue.
---
+++ bin/index.js
@@ -0,0 +1,73 @@
+#! /usr/bin/env node
+import fs from "fs"
+import path from "path"
+
+// List of file types to check
+const fileTypes = ['.js', '.ts', '.jsx', '.tsx']
+
+const blacklisted = ["node_modules", ".yarn", ".pnp.cjs", ".pnp.loader.mjs"]
+
+// Read .env file and store the key-value pairs in an object
+const env = fs
+    .readFileSync('.env', 'utf-8')
+    .split('\n')
+    .filter((line) => line.length > 0 && !line.startsWith('#'))
+    .reduce((acc, curr) => {
+        const [key, value] = curr.split('=')
+        acc[key] = value
+        return acc
+    }, {})
+
+// Function to check a file
+const checkFile = (filePath) => {
+    // Read the file
+    const fileContent = fs.readFileSync(filePath, 'utf-8')
+
+    // Find all occurrences of `process.env.<value>`
+    const envMatches = fileContent.match(/process\.env\.[A-Z0-9_]+/gi)
+
+    // If there are no matches, return
+    if (!envMatches) {
+        return
+    }
+
+    // Iterate over the matches
+    envMatches.forEach((match) => {
+        // Extract the <value> part
+        const envKey = match.split('.')[2]
+
+        // Check if the key is specified in the .env file
+        if (!env[envKey]) {
+            console.log(`Warning: "${envKey}" is not specified in the .env file`)
+            process.exit(1)
+        }
+    })
+}
+
+// Function to check all files in a directory
+const checkDirectory = (dir) => {
+    // Get a list of all files in the directory
+    const files = fs.readdirSync(dir)
+
+    // Iterate over the files
+    files.forEach((filename) => {
+        if (blacklisted.includes(filename)) {
+            return;
+        }
+        // Get the full path of the file
+        const filePath = path.join(dir, filename)
+        // Check if the file is a directory
+        if (fs.lstatSync(filePath).isDirectory()) {
+            // If it's a directory, recursively check the files in it
+            checkDirectory(filePath)
+        } else {
+            // Check if the file has a valid file type and is not blacklisted
+            if (fileTypes.includes(path.extname(filename))) {
+                // Check the file
+                checkFile(filePath)
+            }
+        }
+    })
+}
+
+checkDirectory(".")
